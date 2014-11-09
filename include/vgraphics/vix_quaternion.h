b2eedb447520bb6ea135afdbf92902a6ffc97f02
Matt Guerrette <direct3Dtutorials@gmail.com>
2014-11-08 20:18:50 -0500

    implemented various operators for quat.

--- include/vgraphics/vix_quaternion.h
+++ include/vgraphics/vix_quaternion.h
@@ -2,6 +2,7 @@
 #define VIX_QUATERNION_H
 
 #include <vix_platform.h>
+#include <iostream>
 
 namespace Vixen {
 
@@ -9,14 +10,15 @@ namespace Vixen {
 
 	class VIX_API Quaternion
 	{
+	public:
+		float m_w;
 		float m_x;
 		float m_y;
 		float m_z;
-		float m_w;
 
 	public:
-		inline Quaternion(float x, float y, float z, float w)
-			: m_x(x), m_y(y), m_z(z), m_w(w)
+		inline Quaternion(float w, float x, float y, float z)
+			: m_w(w), m_x(x), m_y(y), m_z(z)
 		{
 		}
 
@@ -25,9 +27,41 @@ namespace Vixen {
 
 		}
 
+		inline Quaternion& operator= (const Quaternion& rhs)
+		{
+			m_w = rhs.m_w;
+			m_x = rhs.m_x;
+			m_y = rhs.m_y;
+			m_z = rhs.m_z;
+
+			return *this;
+		}
+
+		float Norm() const;
+		float Normalize();
+
+		
+		float  operator[] (const size_t i) const;
+		float& operator[] (const size_t i);
+		Quaternion operator+ (const Quaternion& rhs) const;
+		Quaternion operator- (const Quaternion& rhs) const;
+		Quaternion operator* (const Quaternion& rhs) const;
+		Quaternion operator* (float scalar) const;
 
 		static const Quaternion Identity;
 		static const Quaternion Zero;
+
+
+		inline VIX_API friend std::ostream& operator <<
+			(std::ostream& o, const Quaternion& q)
+		{
+			o << "Quaternion [ "
+				<< "w:" << q.m_w << ", "
+				<< "x:" << q.m_x << ", "
+				<< "y:" << q.m_y << ", "
+				<< "z:" << q.m_z << " ]";
+			return o;
+		}
 	};
 
 }
--- source/vgraphics/vix_quaternion.cpp
+++ source/vgraphics/vix_quaternion.cpp
@@ -1,7 +1,88 @@
 #include <vix_quaternion.h>
+#include <cassert>
+#include <math.h>
 
 namespace Vixen {
 
-	const Quaternion Quaternion::Identity(0, 0, 0, 1);
+	const Quaternion Quaternion::Identity(1, 0, 0, 0);
 	const Quaternion Quaternion::Zero(0, 0, 0, 0);
+
+	float Quaternion::Norm() const
+	{
+		float w = m_w * m_w;
+		float x = m_x * m_x;
+		float y = m_y * m_y;
+		float z = m_z * m_z;
+
+		return w + x + y + z;
+	}
+
+	float Quaternion::Normalize()
+	{
+		float norm = Norm();
+		float f = 1.0f / sqrt(norm);
+
+		*this = (*this * f);
+
+		//return the norm
+		return norm;
+	}
+
+	float Quaternion::operator[] (const size_t i) const
+	{
+		assert(i < 4); //only 3 members
+
+		return *(&m_x + i);
+	}
+
+	float& Quaternion::operator[] (const size_t i)
+	{
+		assert(i < 4); //only 3 members
+
+		return *(&m_x + i);
+	}
+
+	Quaternion Quaternion::operator* (float scalar) const
+	{
+		float w = m_w * scalar;
+		float x = m_x * scalar;
+		float y = m_y * scalar;
+		float z = m_z * scalar;
+
+		return Quaternion(w, x, y, z);
+	}
+
+	Quaternion Quaternion::operator* (const Quaternion& rhs) const
+	{
+		float w = 0.0f;
+		float x = 0.0f;
+		float y = 0.0f;
+		float z = 0.0f;
+
+		//calculate new w
+		w = (rhs.m_w * m_w) - 
+			(rhs.m_x * m_x) - 
+			(rhs.m_y * m_y) - 
+			(rhs.m_z * m_z);
+
+		//calculate new x
+		x = (rhs.m_w * m_x) +
+			(rhs.m_x * m_w) -
+			(rhs.m_y * m_z) +
+			(rhs.m_z * m_y);
+
+		//calculate new y
+		y = (rhs.m_w * m_y) +
+			(rhs.m_x * m_z) +
+			(rhs.m_y * m_w) -
+			(rhs.m_z * m_x);
+
+		//calculate new z
+		z = (rhs.m_w * m_z) -
+			(rhs.m_x * m_y) +
+			(rhs.m_y * m_x) +
+			(rhs.m_z * m_w);
+
+		return Quaternion(w, x, y, z);
+	}
 }
